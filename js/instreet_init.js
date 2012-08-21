8b9e4d2d739604b217f921fcdadf8e8eaba3f10e
exterminateUD <513933983@qq.com>
2012-08-21 14:07:57 +0800

    change init.js

--- js/instreet_init.js
+++ js/instreet_init.js
@@ -3,7 +3,10 @@
   var widgetSid="6ed7yugOfTf7BQKm8ShWw0",
 	  actionUrl="http://localhost/action.php",
 	  ifengUrl="js/instreet_ifeng.js";  
-  var ranN=Math.round(Math.random()*1000000),funcName="callback_"+ranN,scriptUrl=actionUrl+"?pd="+widgetSid+"&callback="+funcName; 
+	  
+	  
+  var ranN=Math.round(Math.random()*1000000),funcName="callback_"+ranN;
+  actionUrl+="?pd="+widgetSid+"&pu="+encodeURIComponent(encodeURIComponent(location.href))+"&callback="+funcName; 
   
   window[funcName]=function(data){
       if(data&&typeof data=="object"){
@@ -15,6 +18,6 @@
   
   }
   
-  document.write('<script src="'+scriptUrl+'" charset="utf-8" type="text/javascript"><'+'/script>');
+  document.write('<script src="'+actionUrl+'" charset="utf-8" type="text/javascript"><'+'/script>');
 
 })();
